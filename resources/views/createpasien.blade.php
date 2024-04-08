85a03d9a73b35eebbcbea578429b6b452b3d0488
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-04-08 18:45:01 +0700

    8 april 24

---
+++ resources/views/createpasien.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasien List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<form>
+  <div class="form-group">
+    <label for="namapasien">Nama Pasien</label>
+    <input type="text" class="form-control" id="namapasien" placeholder="nama lengkap">
+  </div>
+  <div class="form-group">
+    <label for="umur">Umur Pasien</label>
+    <input type="text" class="form-control" id="umur" placeholder="umur pasien">
+  </div>
+  <div class="form-group">
+    <label for="jenis_kelamin">Jenis Kelamin</label>
+    <input type="text" class="form-control" id="jenis_kelamin">
+  </div>
+  <div class="form-group">
+  <label for="tanggal_lahir">Tanggal Lahir</label>
+    <input type="text" class="form-control" id="tanggal_lahir">
+  </div>
+  <div class="form-group">
+  <label for="alamat">Alamat</label>
+    <input type="text" class="form-control" id="alamat">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+</body>
+</html>
